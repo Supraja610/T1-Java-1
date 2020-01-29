b9544503c83d39a68395e49c83fd355d5a3761cc
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-01-29 10:59:21 +0530

    Add files via upload

---
+++ RoutingWithAuthV1/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { ProductBaseComponent } from './product-base/product-base.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+
+
+// array of Route objects
+// each route object holds the relation between url and component
+const routes: Routes = [
+    // Route object
+    { path:"", redirectTo : "home", pathMatch: "full"},
+    { path:"home", component: HomeComponent},
+    { path:"contactus", component: ContactComponent},
+    { path:"aboutus", component: AboutComponent},
+    { path:"product", component: ProductBaseComponent},
+    { path:"login", component: LoginComponent},
+    { path:"logout", component: LogoutComponent},
+    // <url>/:<name by which data is accessed>
+    { path:"search/:searchText", component: SearchResultComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], // registering routes
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
