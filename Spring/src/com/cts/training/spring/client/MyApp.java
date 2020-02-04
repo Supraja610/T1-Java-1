c2ae80a0c724253f5a57841e925adb99da4ff7f0
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-02-04 13:33:17 +0530

    Add files via upload

--- Spring/src/com/cts/training/spring/client/MyApp.java
+++ Spring/src/com/cts/training/spring/client/MyApp.java
@@ -13,7 +13,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// fetch an instance of bean factory based on config file
+		// created and fect fetch an instance of bean factory based on config file
 		// need to specify which xml to read for config
 		ClassPathXmlApplicationContext context = 
 							new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -22,6 +22,14 @@ public class MyApp {
 		IMessageService service = context.getBean("msgService", IMessageService.class);
 		String ack = service.sendMessage("someone", "Hello");
 		System.out.println(ack);
+		
+		// fetch the object
+		IMessageService servicePro = context.getBean("msgService", IMessageService.class);
+		ack = servicePro.sendMessage("someone", "Hello");
+		System.out.println(ack);
+		
+		// when no more required close the instance of bean factory
+		context.close();
 	}
 
 }
--- Spring/src/com/cts/training/spring/services/EmailService.java
+++ Spring/src/com/cts/training/spring/services/EmailService.java
@@ -3,8 +3,27 @@ package com.cts.training.spring.services;
 public class EmailService implements IMessageService{
 	
 	// add dependency
+	
 	// which fortune service : decided in config
 	private IFortuneService fortuneService;
+	
+	// sender literal value is to be injected by spring-context
+	private String sender;
+	
+	private static int instances;
+	static {
+		instances = 0;
+	}
+	
+	public EmailService() {
+		System.out.println("\n**********Email Object created*********");
+		EmailService.instances++;
+	}
+	
+	
+	
+	
+	
 	
 	/************ Constructor Based DI **************/
 	/*
@@ -20,10 +39,25 @@ public class EmailService implements IMessageService{
 		this.fortuneService = fortuneService;
 	}
 	
+	/***************  Setter Based DI *************/
+	// param sender will be injected by spring-context
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	
+	
+	public void postConstruct() {
+		System.out.println("\n*******Post Construct called********\n");
+	}
 	
 	@Override
 	public String sendMessage(String to, String message) {
-		return "Email sent to : " + to + "[ * " + message + " * ]"
+		return "Email(" + EmailService.instances + ") sent to : " + to + "[ * " + message + " * ]"
+				+ "\nSent By : " + this.sender
 				+ "\n" + this.fortuneService.dailyFortune();
+	}
+	
+	public void preDestroy() {
+		System.out.println("\n*******Pre Destroy called********\n");
 	}
 }
