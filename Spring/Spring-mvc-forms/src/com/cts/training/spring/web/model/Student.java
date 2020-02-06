19c62554d8ce2914f8d7579411aa0d034471773f
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-02-06 15:39:13 +0530

    Add files via upload

--- Spring/Spring-mvc-forms/src/com/cts/training/spring/web/model/Student.java
+++ Spring/Spring-mvc-forms/src/com/cts/training/spring/web/model/Student.java
@@ -1,9 +1,27 @@
 package com.cts.training.spring.web.model;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String uname;
 	private String email;
+	private String country;
+	private String favoriteLanguage;
+	private String[] operatingSystems;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+		// dummy list of countries (can be fetched from DB)
+		this.countries = new LinkedHashMap<String, String>();
+		this.countries.put("FR", "FRANCE");
+		this.countries.put("IN", "INDIA");
+		this.countries.put("GR", "GERMANY");
+		this.countries.put("US", "USA");
+	}
+	
+	// collection to hold country list
+	private LinkedHashMap<String, String> countries;
 	
 	public String getUname() {
 		return uname;
@@ -21,6 +39,30 @@ public class Student {
 		if(this.email == null)
 			this.email = "dummy@mail.com";
 	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	// used by the form
+	public LinkedHashMap<String, String> getCountries() {
+		return countries; 
+	}
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+	
+	
 	
 	
 }
