0786b7cd2cbf9c24fe0d60e24f660d473988723b
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-02-06 10:38:34 +0530

    Add files via upload

--- Spring/Spring-mvc/src/com/cts/training/spring/web/controller/StudentController.java
+++ Spring/Spring-mvc/src/com/cts/training/spring/web/controller/StudentController.java
@@ -1,7 +1,12 @@
 package com.cts.training.spring.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cts.training.spring.web.model.Student;
 
 @Controller
 @RequestMapping("/student")
@@ -17,6 +22,41 @@ public class StudentController {
 		return "student-profile";
 	}
 	
+	// query String
+	// http://localhost:9090/Spring-mvc/student/save?uname=First&email=first@mail.com
+	// Traditional Servlet way
+	/*@RequestMapping("/save") // fallback method for bad url
+	public String saveData(HttpServletRequest request) {
+		String uname = request.getParameter("uname");
+		String email = request.getParameter("email");
+		System.out.println("\nName : " + uname);
+		System.out.println("Email : " + email);
+		return "student-confirm";
+	}*/
+	
+	// Spring way
+	/*@RequestMapping("/save") // fallback method for bad url
+	// public String saveData(@RequestParam String uname, @RequestParam String email) {
+	public String saveData(@RequestParam("uname") String userName, @RequestParam(value = "email", required = false, defaultValue = "dummy@mail.com") String userEmail) {
+		// System.out.println("\nName : " + uname);
+		// System.out.println("Email : " + email);
+		
+		System.out.println("\nName : " + userName);
+		System.out.println("Email : " + userEmail.toUpperCase());
+		return "student-confirm";
+	}*/
+	
+	// Spring way (using model class)
+	@RequestMapping("/save") // fallback method for bad url
+	public String saveData(Student student) {
+		System.out.println("\nName : " + student.getUname());
+		System.out.println("Email : " + student.getEmail());
+		return "student-confirm";
+	}
+	
+	
+	
+	
 	// mapping action method with parent url
 	@RequestMapping() // default method for parent url
 	public String defaultMethod() {
@@ -28,6 +68,8 @@ public class StudentController {
 	public String fallbackMethod() {
 		return "student-error";
 	}
+	
+	
 }
 
 
